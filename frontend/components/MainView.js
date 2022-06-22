0750852abfa71bf85e84269f5c2d845cc9377bb9
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2022-06-22 17:15:25 -0400

    still setting up MainView

--- frontend/components/MainView.js
+++ frontend/components/MainView.js
@@ -1,5 +1,23 @@
 import React from 'react'
 import Signup from './Signup'
+import {useEffect, useState} from 'react'
+import {useWallet} from '@solana/wallet-adapter-react'
+import {SOLANA_HOST} from '../utils/const'
+import {getProgramInstance} from '../utils/utils'
+import {TOKEN_PROGRAM_ID} from '@solana/spl-token'
+
+import useAccount from '../hooks/useAccount'
+
+const anchor = require('@project-serum/anchor')
+const utf8 = anchor.utils.bytes.utf8
+const {BN, web3} = anchor
+const {SystemProgram} = web3
+
+const defaultAccounts = {
+  tokenProgram: TOKEN_PROGRAM_ID,
+  clock: anchor.web3.SYSVAR_CLOCK_PUBKEY,
+  systemProgram: SystemProgram.programId,
+}
 
 let isAccount = false
 
